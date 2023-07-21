e0033445716545f0209795f92ea54136226df4b3
pinchao <cpc4253@163.com>
2023-07-21 18:43:03 +0800

    函数中的泛型

--- src/generic_demo.rs
+++ src/generic_demo.rs
@@ -34,4 +34,26 @@ pub fn run() {
     //     Ok(T),
     //     Err(E),
     // }
+
+    // 函数中的泛型
+    fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+        let mut largest = &list[0];
+
+        for item in list {
+            if item > largest {
+                largest = item;
+            }
+        }
+
+        largest
+    }
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 }
