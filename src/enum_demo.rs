ee78b5880a2af07228e22d84e83204148edc12a8
pinchao <cpc4253@163.com>
2023-07-21 18:58:03 +0800

    枚举类型与错误处理

--- src/enum_demo.rs
+++ src/enum_demo.rs
@@ -1,1 +1,35 @@
-pub fn run() {}
+use std::fs::File;
+
+pub fn run() {
+    /*
+     * enum Option<T> {
+     *   Some<T>,
+     *   None
+     * }
+     */
+    let a = Some(5);
+    match a {
+        Some(value) => println!("The value is: {}", value),
+        None => println!("The value is None"),
+    }
+
+    let b = Some(10).unwrap();
+    println!("The value is: {}", b);
+
+    /*
+     * enum Result<T, E> {
+     *   Ok<T>,
+     *   Error(#)
+     * }
+     */
+    // let f1 = File::open("hello.txt").expect("open file error");
+    // println!("{:#?}", f1);
+
+    let f2 = File::open("hello.txt");
+    match f2 {
+        Ok(file) => println!("{:#?}", file),
+        Err(error) => {
+            panic!("open file error: {:#?}", error);
+        }
+    }
+}
